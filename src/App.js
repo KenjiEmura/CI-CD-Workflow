44f91bd3539ac87a3fdf635cd5c7edabe0d4fc43
Kenji Emura <kenjiemura@gmail.com>
2021-05-31 17:37:30 +0900

    Signed commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Changed something again...
+          Changed something again and again...
         </a>
       </header>
     </div>
