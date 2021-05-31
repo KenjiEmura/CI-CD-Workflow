c643279c1991a524af7e190e90dbb03518d928c6
Kenji Emura <kenjiemura@gmail.com>
2021-05-31 16:42:20 +0900

    Test commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Kenji's React App
+          Changed something...
         </a>
       </header>
     </div>
