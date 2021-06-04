06215353f1a682eb7e3c70294fec1d78dd03a24c
Kenji Emura <kenjiemura@gmail.com>
2021-06-05 02:12:40 +0900

    BREAKING CHANGE: Dummy commit simulating a major upgrade
    What I really did was just adding a new line to the home screen that says that this is the version 2.0

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           This is my <code>CI/CD</code> dummy React Project
         </p>
+        <p>App version 2.0</p>
         <a
           className="App-link"
           href="https://reactjs.org"
