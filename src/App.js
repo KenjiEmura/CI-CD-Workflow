26f6c986fadf162c9bab24f69ae17ae6449c16ae
Kenji Emura <kenjiemura@gmail.com>
2021-06-06 20:58:23 +0900

    fix: Fixed the auto induced error

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Kenji's React
+          Kenji's React App
         </a>
       </header>
     </div>
