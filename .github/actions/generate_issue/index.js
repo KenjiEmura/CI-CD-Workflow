93947993a9bd3e7767bbcef0a4927fc00abba2f7
Kenji Emura <kenjiemura@gmail.com>
2021-06-07 13:40:57 +0900

    ci: added await to the octokit function:
    Signed-off-by: Kenji Emura <kenjiemura@gmail.com>

--- .github/actions/generate_issue/index.js
+++ .github/actions/generate_issue/index.js
@@ -15,7 +15,7 @@ try {
   // The octokit lives inside our github package
   // https://octokit.github.io/rest.js/v18#issues
   const octokit = new github.getOctokit(token);
-  const response = octokit.rest.issues.create({
+  const response = await octokit.rest.issues.create({
     // owner: github.context.repo.owner,
     // repo: github.context.repo.repo,
     ...github.context.repo,
