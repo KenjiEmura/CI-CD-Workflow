078c62ad4a141d8e33a7d6699b6870a58d7c15b1
Kenji Emura <kenjiemura@gmail.com>
2021-06-07 13:31:14 +0900

    ci: Updated the octokit constructor
    The newest version of @actions/github no longer supports creating an instance of octokit using GitHub(), the new syntax is .getOctokit()

--- .github/actions/generate_issue/index.js
+++ .github/actions/generate_issue/index.js
@@ -14,7 +14,7 @@ try {
 
   // The octokit lives inside our github package
   // https://octokit.github.io/rest.js/v18#issues
-  const octokit = new github.GitHub(token);
+  const octokit = new github.getOctokit(token);
   const response = octokit.rest.issues.addAssignees({
     // owner: github.context.repo.owner,
     // repo: github.context.repo.repo,
