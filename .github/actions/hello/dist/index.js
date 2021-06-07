e8414507bf385dad4b4161511a9eb586265dc735
Kenji Emura <kenjiemura@gmail.com>
2021-06-07 11:46:57 +0900

    ci: Added custom env variable and @actions/core logs

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -1292,20 +1292,47 @@ module.exports = /******/ (function (modules, runtime) {
       __unusedexports,
       __webpack_require__
     ) {
+      // This file is being compiled using the next command:
+      // `npx ncc build .github/actions/hello/index.js -o .github/actions/hello/dist`
+      // We use the previous command to build a unified javascript file that will include
+      // all the code of the packages that we need (in this case, @actions/core and @actions/github)
+
       const core = __webpack_require__(470);
       const github = __webpack_require__(469);
 
       try {
-        throw new Error("Sample error");
-        const name = core.getInput("who-to-greet");
-        console.log(`Hello ${name}`);
+        // throw new Error("Sample error");
 
-        const time = new Date();
-        core.setOutput("time", time.toTimeString());
+        // We can log things to the output console if the ACTIONS_STEP_DEBUG is set to true in the secrets (repository settings)
+        core.debug("Debug message");
+        core.warning("Warning message");
+        core.error("Error message");
+
+        // We can choose which variables should be hidden if accidentally output
+        const theSecret = "The secret...";
+        core.setSecret(theSecret);
+        core.debug(`The secret is ${theSecret}`);
+        console.log(`The secret is ${theSecret}`);
+
+        // We can set an expandable behaviour on objects that are too long
+        core.startGroup("Logging the github object");
         console.log(
           "The @actions/github object: ",
           JSON.stringify(github, null, "\t")
         );
+        core.endGroup;
+
+        // We can set env variables that can be used in any subsequent steps
+        core.exportVariable(
+          "CUSTOM_ENV_VARIABLE",
+          "This variable was created inside the /actions/hello/index.js file"
+        );
+
+        const name = core.getInput("who-to-greet");
+        console.log(`Hello ${name}`);
+        const time = new Date();
+        core.setOutput("time", time.toTimeString());
+
         console.log(
           "The @actions/core object: ",
           JSON.stringify(core, null, "\t")
--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -1,17 +1,44 @@
+// This file is being compiled using the next command:
+// `npx ncc build .github/actions/hello/index.js -o .github/actions/hello/dist`
+// We use the previous command to build a unified javascript file that will include
+// all the code of the packages that we need (in this case, @actions/core and @actions/github)
+
 const core = require("@actions/core");
 const github = require("@actions/github");
 
 try {
-  throw new Error("Sample error");
-  const name = core.getInput("who-to-greet");
-  console.log(`Hello ${name}`);
+  // throw new Error("Sample error");
 
-  const time = new Date();
-  core.setOutput("time", time.toTimeString());
+  // We can log things to the output console if the ACTIONS_STEP_DEBUG is set to true in the secrets (repository settings)
+  core.debug("Debug message");
+  core.warning("Warning message");
+  core.error("Error message");
+
+  // We can choose which variables should be hidden if accidentally output
+  const theSecret = "The secret...";
+  core.setSecret(theSecret);
+  core.debug(`The secret is ${theSecret}`);
+  console.log(`The secret is ${theSecret}`);
+
+  // We can set an expandable behaviour on objects that are too long
+  core.startGroup("Logging the github object");
   console.log(
     "The @actions/github object: ",
     JSON.stringify(github, null, "\t")
   );
+  core.endGroup;
+
+  // We can set env variables that can be used in any subsequent steps
+  core.exportVariable(
+    "CUSTOM_ENV_VARIABLE",
+    "This variable was created inside the /actions/hello/index.js file"
+  );
+
+  const name = core.getInput("who-to-greet");
+  console.log(`Hello ${name}`);
+  const time = new Date();
+  core.setOutput("time", time.toTimeString());
+
   console.log("The @actions/core object: ", JSON.stringify(core, null, "\t"));
 } catch (error) {
   core.setFailed(error.message);
